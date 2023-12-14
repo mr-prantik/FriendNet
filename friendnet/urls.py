da7cbbe746eab7baa36aee3a99b1ed80e237a157
mr-prantik <prantikmahata9211@gmail.com>
2023-12-14 19:27:49 +0530

    updated LICENSE

--- friendnet/urls.py
+++ friendnet/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path as url
 
 urlpatterns = [
     path("admin/", admin.site.urls),

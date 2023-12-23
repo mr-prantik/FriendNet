77cb0370f0544577f58a55a77fa7f95e6bdce6a2
mr-prantik <prantikmahata9211@gmail.com>
2023-12-23 17:29:50 +0530

    updated

--- friendnet/settings.py
+++ friendnet/settings.py
@@ -6,14 +6,10 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
